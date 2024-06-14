73b0347e9be224b27ee416ab1ca4bed690add54f
orshmuel9 <orshmuel9@gmail.com>
2024-06-14 19:33:15 +0300

    part1

--- part1.c
+++ part1.c
@@ -10,6 +10,10 @@ int main(int argc ,char *argv[]) {
         fprintf(stderr, "Usage: %s <parent_message> <child1_message> <child2_message> <count>", argv[0]);
         return 1;
     }
+    int count = atoi(argv[4]);
+    char *parent_message = argv[1];
+    char *child1_message = argv[2];
+    char *child2_message = argv[3];
     FILE *file = fopen("output.txt", "w");
     if (file == NULL) {
         perror("fopen");
@@ -21,36 +25,35 @@ int main(int argc ,char *argv[]) {
         return 1;
     }
     if(pid == 0){
+        for(int i = 0; i < count; i++){
+            fprintf(file, "%s\n", child1_message);
+        }
+        fclose(file);
+        exit (0);
+    }else{
+        wait(NULL);
         int pid2 = fork();
         if (pid2 < 0) {
             perror("fork failed");
             return 1;
         }
         if(pid2 == 0){
-            for(int i = 0; i < atoi(argv[4]); i++){
-                //printf("%s\n", argv[3]);
-                fprintf(file, "%s\n", argv[2]);
-            }
-            fclose(file);
-            exit (0);
-        }else{
             sleep(1);
-            for(int i = 0; i < atoi(argv[4]); i++){
-                //printf("%s\n", argv[2]);
-                fprintf(file, "%s\n", argv[3]);
+            for(int i = 0; i < count; i++){
+                fprintf(file, "%s\n", child2_message);
             }
             fclose(file);
-            wait(NULL);
             exit (0);
         }
-    }else{
-        wait(NULL);
-        wait(NULL);
-        for(int i = 0; i < atoi(argv[4]); i++){
-            //printf("%s\n", argv[1]);
-            fprintf(file, "%s\n", argv[1]);
+        else{
+            wait(NULL);
+            sleep(1);
+            for(int i = 0; i < count; i++){
+                fprintf(file, "%s\n", parent_message);
+            }
+            fclose(file);
+            exit (0);
         }
-        exit (0);
     }
     fclose(file);
     return 0;
