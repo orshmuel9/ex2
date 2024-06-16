01d0394fcb30d93e09ce1de40dd0be79248c11bf
orshmuel9 <orshmuel9@gmail.com>
2024-06-16 18:42:46 +0300

    part 2 hope done

--- part2.c
+++ part2.c
@@ -14,7 +14,7 @@ void write_message(const char *message, int count) {
 }
 
 int main(int argc, char *argv[]) {
-    if (argc < 6) {
+    if (argc < 4) {
         fprintf(stderr, "Usage: %s <message1> <message2> ... <order> <count>\n", argv[0]);
         return 1;
     }
@@ -22,16 +22,16 @@ int main(int argc, char *argv[]) {
     const char *lockfile = "lockfile.lock";
     unlink(lockfile);
     int num_times = atoi(argv[argc - 1]);
-    int num_forks = argc - 3;
+    int num_forks = argc - 2;
 
     pid_t pids[num_forks];
-    char *order = argv[argc - 2];
-    int real_order[num_forks];
-    char *token = strtok(order, " ");
-    for (int i = 0; i < num_forks; i++) {
-        real_order[i] = atoi(token) - 1;
-        token = strtok(NULL, " ");
-    }
+    // char *order = argv[argc - 2];
+    // int real_order[num_forks];
+    // char *token = strtok(order, " ");
+    // for (int i = 0; i < num_forks; i++) {
+    //     real_order[i] = atoi(token) - 1;
+    //     token = strtok(NULL, " ");
+    // }
 
     for (int i = 0; i < num_forks; i++) {
         pids[i] = fork();
@@ -40,41 +40,24 @@ int main(int argc, char *argv[]) {
             return 1;
         }
         if (pids[i] == 0) {
-            const char *message = argv[real_order[i] + 1];
-
-            // Remove the literal \n from the message
-            // char clean_message[256];
-            // strncpy(clean_message, message, sizeof(clean_message) - 1);
-            // clean_message[sizeof(clean_message) - 1] = '\0';
-            // char *newline_pos = strstr(clean_message, "\\n");
-            // if (newline_pos) {
-            //     *newline_pos = '\0'; // Remove the \n
-            // }
-
-            for (int j = 0; j < num_times; j++) {
-                // Acquire lock
-                while (1) {
-                    int fd = open(lockfile, O_CREAT | O_EXCL, 0666);
-                    if (fd != -1) {
-                        close(fd);
-                        break;
+            const char *message = argv[i+1];
+            while (1) {
+                int fd = open(lockfile, O_CREAT | O_EXCL, 0666);
+                if (fd != -1) {
+                    close(fd);
+                    break;
                     }
                     usleep(1000); // Wait 1 millisecond before retrying
-                }
-
-                // Write to stdout
-                write_message(message, 1);  
-                printf("\n"); 
-                fflush(stdout);  
-
-                // Release lock
-                unlink(lockfile);
             }
+            // Write to stdout
+            write_message(message, num_times);   
+            fflush(stdout);  
+            // Release lock
+            unlink(lockfile);
             exit(0);
         }
         sleep(2);
     }
-
     // Parent process
     for (int i = 0; i < num_forks; i++) {
         waitpid(pids[i], NULL, 0);
